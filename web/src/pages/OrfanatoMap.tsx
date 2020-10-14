01c1d1ed758da16b0df41c63fd77d143ac07aaad
ArthurMaverick <arthuracs18@gmail.com>
2020-10-14 10:42:19 -0300

    nlw3-day3

--- web/src/pages/OrfanatoMap.tsx
+++ web/src/pages/OrfanatoMap.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import mapMarkerImg from '../images/map-marker.svg'
-import {FiPlus} from 'react-icons/fi'
+import {FiPlus, FiArrowRight} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
+import {Map, TileLayer, Marker , Popup} from 'react-leaflet'
+import Leaflet from 'leaflet'
 import '../styles/pages/orfanato-map.css'
-import {Map, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+
 const urlMap = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token='
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58,68],
+  iconAnchor: [29,68],
+  popupAnchor: [170, 2]
+})
 
 function OrfanatoMap() {
 
@@ -34,6 +42,21 @@ function OrfanatoMap() {
         style={{width: '100%', height: '100%'}}
       >
         <TileLayer url={`${urlMap}${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+
+        <Marker
+          icon={mapIcon}
+          position={[-22.9387205,-44.4028928]}
+        >
+            
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+            lar das meninas
+            <Link to="">
+              <FiArrowRight size={20} color='#fff'/>
+            </Link>
+          </Popup>
+
+        </Marker>
+
       </Map>
 
       <Link to="" className="create-orfanato">
