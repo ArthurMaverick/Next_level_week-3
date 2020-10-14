58c6cf396fd4cb975988ee777f883d5d06d66520
ArthurMaverick <arthuracs18@gmail.com>
2020-10-14 17:20:08 -0300

    nlw3-day3

--- web/src/pages/OrfanatoMap.tsx
+++ web/src/pages/OrfanatoMap.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import mapMarkerImg from '../images/map-marker.svg'
 import {FiPlus, FiArrowRight} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
 import {Map, TileLayer, Marker , Popup} from 'react-leaflet'
-import Leaflet from 'leaflet'
+
 import '../styles/pages/orfanato-map.css'
-import 'leaflet/dist/leaflet.css'
+import mapIcon from '../utils/mapIcon'
+import api from '../services/ap';
 
 const urlMap = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token='
-const mapIcon = Leaflet.icon({
-  iconUrl: mapMarkerImg,
-  iconSize: [58,68],
-  iconAnchor: [29,68],
-  popupAnchor: [170, 2]
-})
+
+interface Orfanato {
+  id: number
+  latitude: number
+  longitude: number
+  name: string
+}
 
 function OrfanatoMap() {
+  const [orfanatos, setOrfanatos] = useState<Orfanato[]>([])
 
+    
+  useEffect(() => {
+    api.get('orfanatos').then(response => {
+      setOrfanatos(response.data)
+      // console.log(response.data[0].latitude);
+    });
+  }, []);
+  
   return (
     <div id="page-map">
       <aside>
@@ -40,23 +51,30 @@ function OrfanatoMap() {
         center={[-22.9387205,-44.4028928]}
         zoom={11}
         style={{width: '100%', height: '100%'}}
-      >
+        >
         <TileLayer url={`${urlMap}${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
 
-        <Marker
-          icon={mapIcon}
-          position={[-22.9387205,-44.4028928]}
-        >
+        
+        {orfanatos.map(orfanato =>{
+          return (
+            
+            <Marker
+            icon={mapIcon}
+            position={[orfanato.latitude, orfanato.longitude]}
+            key={orfanato.id}
+            >
+            
             
           <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
-            lar das meninas
-            <Link to="orfanatos/1">
+            {orfanato.name}
+            <Link to={`orfanatos/${orfanato.id}`}>
               <FiArrowRight size={20} color='#fff'/>
             </Link>
           </Popup>
 
         </Marker>
-
+          )
+        })}
       </Map>
 
       <Link to="/orfanatos/create" className="create-orfanato">
